51e31e88fd4bfc247018ba5bcef4f9aee2b8182b
JessonGuitar <jessonyang1999@outlook.com>
2026-05-18 15:43:08 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
   PROXIO_BLOG_TITLE: '文章',
   PROXIO_BLOG_COUNT: 5, // 首页博文区块展示前5篇文章
-  PROXIO_BLOG_TEXT_1: '简介 ･ 音乐 ･ 课程 ･ 著作 ･ 随笔',
+  PROXIO_BLOG_TEXT_1: '简介 ･ 音乐 ･ 课程 ･ 著作',
 
   // 区块默认内容显示文章的summary文本，但也支持用自定义图片或logo德国替换掉占位显示内容
   PROXIO_BLOG_PLACEHOLDER_IMG_URL_1: '', // 填写要替换成的图片，支持图床或直接上传到项目中，示例  /images/feature-1.webp
